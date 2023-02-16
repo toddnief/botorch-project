7dbcc93b9043b74c7aae2714d91185e90931254f
toddnief <todd.nief@gmail.com>
2023-02-15 22:42:18 -0600

    figure out path issues on midway

--- vae_test.py
+++ vae_test.py
@@ -2,8 +2,8 @@
 from os import environ
 environ['KERAS_BACKEND'] = 'tensorflow'
 # vae stuff
-from chemical_vae.chemvae.vae_utils import VAEUtils
-from chemical_vae.chemvae import mol_utils as mu
+# from chemical_vae.chemvae.vae_utils import VAEUtils
+# from chemical_vae.chemvae import mol_utils as mu
 # import scientific py
 import numpy as np
 import pandas as pd
@@ -15,4 +15,6 @@ import matplotlib.pyplot as plt
 import matplotlib as mpl
 from IPython.display import SVG, display
 
-print("lol")
+import sys
+
+print(sys.path)
